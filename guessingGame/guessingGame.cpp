838ec8105348d55f84d87a6cfc9b047e6668ed18
jasonxshi <322023@SHS-10DCE1254.beaverton.k12.or.us>
2017-09-15 11:45:14 -0700

    added comments

--- guessingGame/guessingGame.cpp
+++ guessingGame/guessingGame.cpp
@@ -1,29 +1,44 @@
+/**
+Jason Shi
+9/15/17
+Guessing Game
+Lets the user play "guessing game"
+ **/
+
+//import libraries
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
 int main(){
   while(true){
+    //initialize variables
     int count = 0;
     int randNum;
     int input = 0;
     char playAgain;
+    //find random number
     srand (time(NULL));
     randNum = rand() % 101;
     cout << "Input your guess:";
+    //run game
     while(true){
       cin >> input;
       count++;
+      //if number is too high or low 
       if(input>randNum){
 	cout << "Too high, try again:";
       }else if(input<randNum){
 	cout << "Too low, try again:";
+      //if the guess is correct, prompt play again
       }else{
 	cout << "Correct guess! "<<count <<" Guesses Used. Play again? (y/n)";
 	cin >> playAgain;
 	if(playAgain == 'y'){
+	  //break and restart program
 	  break;
 	}else if(playAgain == 'n'){
+	  //exit program
 	  return(0);
 	}else{
 	  cout << "Not a valid entry, exiting..";
