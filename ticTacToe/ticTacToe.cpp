26996e4c691f1918095fe05f167a2dd155afc4d5
jasonxshi <322023@SHS-10DCE1254.beaverton.k12.or.us>
2017-09-27 11:18:35 -0700

    tictactoe

--- ticTacToe/ticTacToe.cpp
+++ ticTacToe/ticTacToe.cpp
@@ -1,5 +1,31 @@
 /**
 Jason Shi
-TicTacToe
 This program allows you to play ticTacToe 
 **/
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+boolean checkWin(int player);
+void printBoard();
+boolean checkTie();
+void gameFinish();
+
+int main(){
+  int board[3][3];
+  int BLANK = 0;
+  int X_MOVE = 1;
+  int O_MOVE = 2;
+  int X_TURN = 0;
+  int O_TURN= 1;
+  int turn = X_TURN;
+  int xWins = 0;
+  int oWins = 0;
+  char input[2];
+
+  boolean stillPlaying = true;
+  while{stillPlaying){
+    while(!checkWin(X_MOVE) && !checkWin(O_MOVE) && !checkTie()){
+      
+    }
+  }
+}
