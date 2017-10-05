92031d9b7900fdda5d67f1136af00b4dde5b9490
jasonxshi <322023@SHS-10DCE1254.beaverton.k12.or.us>
2017-10-05 12:24:08 -0700

    update

--- ticTacToe/ticTacToe.cpp
+++ ticTacToe/ticTacToe.cpp
@@ -6,10 +6,10 @@ This program allows you to play ticTacToe
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-boolean checkWin(int player);
-void printBoard();
-boolean checkTie();
-void gameFinish();
+bool checkWin(int board[3][3], int player);
+void printBoard(int board[3][3]);
+bool checkTie(int board[3][3]);
+void gameFinish(int board[3][3]);
 
 int main(){
   int board[3][3];
@@ -22,10 +22,10 @@ int main(){
   int xWins = 0;
   int oWins = 0;
   char input[2];
-  boolean stillPlaying = true;
+  bool stillPlaying = true;
   while(stillPlaying){
-    while(!checkWin(X_MOVE) && !checkWin(O_MOVE) && !checkTie()){
-      printBoard();
+    while(!checkWin(board, X_MOVE) && !checkWin(board, O_MOVE) && !checkTie(board)){
+      printBoard(board);
       cout << "Enter move: ";
       cin.get(input, 2);
       cin.get();
@@ -40,18 +40,51 @@ int main(){
 	  turn = X_TURN;	
 	}
       }
+      cout << "There is already a piece there. Choose another spot: ";
+    }
+    printBoard(board);
+    if(checkWin(X_MOVE)){
+      cout>>"X Wins!";
+      xWins++;
+      gameFinish(board);
+    }else if(checkWin(board, O_MOVE)){
+      cout>>"O Wins!";
+      gameFinish(board);
+    }else if(checkTie(board)){
+      cout>>"There is a tie.";
+      gameFinish(board);
+    }
+    cout>>"Play again?";
+    c.get(input[0],1);
+    c.get();
+    if(input[0] == 'y'){
+    }else{
+      stillPlaying = false;
+      cout >> "Thanks for playing!";
     }
   }
 }
-boolean checkTin(int player){
-
+bool checkWin(int board[3][3], int player){
+  for(int row = 0; row<3; row++){
+    if(board[row][column] == player &&board[row][column+1] &&board[row][column+2] == player)
+      return true; 
+  }
+  for(int column=0;column<3;column++){
+    if(board[row][column] == player && board[row+1][column] && board[row+2][column])
+      return true;
+  }
+  if
+  
 }
 
-void printBoard(){
-
+void printBoard(int board[3][3]){
+  // cout <<'\t'<<"a"<<'\t'<<"b"<<'\t'<<"c";
+  // cout <<'1' <<'\t'<<board[0][0]<<'\t'<<board[1][0]<<'\t'<<board[2][0];
+  
+  
 }
 
-boolean checkTie(){
+bool checkTie(int board[3][3]){
   
 }
 
