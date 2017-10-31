8b4c3df3bc5fd0c854af294c6538767a2f04b1b1
jasonxshi <322023@SHS-10DCE1254.beaverton.k12.or.us>
2017-10-31 12:20:32 -0700

    test

---
+++ Classes/digitalMediaImplement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include "digitalMedia.h"
+
+using namespace std;
+
+digitalMedia::digitalMedia(char* newTitle, int newYear){
+  title = newTitle;
+  year = newYear;
+}
+
+char* digitalMedia::getTitle(){
+  return title;
+}
+
+int digitalMedia::getYear(){
+  return year;
+}
